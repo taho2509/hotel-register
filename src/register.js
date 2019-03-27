15cdba562591669d7c4b134041e2d26dbc15e6d6
Angel Ulise Tabares Gonzalez <taho2509@gmail.com>
2019-03-27 12:36:05 -0400

    [FIX] - refactoring exist query in an async await style

--- src/register.js
+++ src/register.js
@@ -3,35 +3,48 @@
 const { MongoClient } = require('mongodb');
 var url = process.env.MONGO_CONNECTION_STRING;
 
-var registrationHandler = function(data) {
+let clientExist = (data) => new Promise((resolve, reject) => {
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db('register');
+  
+      dbo.collection('client')
+        .findOne({ name: data.name, lastname: data.lastname }, function(
+          err,
+          result
+        ) {
+          db.close();
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result !== null);
+          }
+        });
+      
+    });
+  });
+
+var registrationHandler = async function(data) {
 
   var client = { _id: data.id, name: data.name, lastname: data.lastname };
 
-  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db('register');
+  let exist = await clientExist(data);
 
-    dbo.collection('client')
-      .findOne({ name: data.name, lastname: data.lastname }, function(
-        err,
-        result
-      ) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        if(result === null) {
-          dbo.collection('client').insertOne(client, function(err, ans) {
-            if (err) throw err;
-            console.log(ans.insertedCount + ' Client inserted.');
-            console.log('location: ', '/registers/' + ans.insertedId);
-          });
-        } else {
-          console.log('the client already exist');
-        }
+  if(!exist) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db('register');
+      
+      dbo.collection('client').insertOne(client, function(err, ans) {
+        if (err) throw err;
+        console.log(ans.insertedCount + ' Client inserted.');
+        console.log('location: ', '/registers/' + ans.insertedId);
+        db.close();
       });
-    
-  });
+    });
+  } else {
+    console.log('the client already exist');
+  }
 };
 
 module.exports = registrationHandler;
